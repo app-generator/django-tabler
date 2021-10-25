803cbfc2bded7ef7297694e753fa1bd1d102bb40
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2021-10-24 23:34:57 -0300

    problema com o create

--- app/urls.py
+++ app/urls.py
@@ -5,17 +5,18 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path, re_path
-from app import views
+from app.views import index, cadastro_setores,cadastro_equipes,cadastro_fornecedores,cadastro_dpo,cadastro_empresa,create
 
 urlpatterns = [
     # Matches any html file 
-    re_path(r'^.*\.html', views.pages, name='pages'),
+    #re_path(r'^.*\.html', views.pages, name='pages'),
 
     # The home page
-    path('', views.index, name='home'),
-    path('cadastro_setores/',views.cadastro_setores, name='cadastro_setores'),
-    path('cadastro_equipes/',views.cadastro_equipes, name='cadastro_equipes'),
-    path('cadastro_fornecedores/',views.cadastro_fornecedores, name='cadastro_fornecedores'),
-    path('cadastro_dpo/',views.cadastro_dpo, name='cadastro_dpo'),
-    path('cadastro_empresa/',views.cadastro_empresa, name='cadastro_empresa')
+    path('', index, name='home'),
+    path('cadastro_setores/',cadastro_setores, name='cadastro_setores'),
+    path('cadastro_equipes/',cadastro_equipes, name='cadastro_equipes'),
+    path('cadastro_fornecedores/',cadastro_fornecedores, name='cadastro_fornecedores'),
+    path('cadastro_dpo/',cadastro_dpo, name='cadastro_dpo'),
+    path('cadastro_empresa/',cadastro_empresa, name='cadastro_empresa'),
+    path('create/', create, name='create')
 ]
--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponse
 from django import template
 from app.forms import Form_cad_empresa,Form_cad_setores
 
+
 @login_required(login_url="/login/")
 def index(request):
     return render(request, "index.html")
@@ -38,6 +39,14 @@ def cadastro_empresa(request):
     data = {}
     data['form'] = Form_cad_empresa()
     return render(request,"cadastro-empresa.html",data)
+
+@login_required(login_url="/login/")
+def create(request):
+    form = Form_cad_empresa(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('cadastro_empresa')
+
 
 @login_required(login_url="/login/")
 def pages(request):
