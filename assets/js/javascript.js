de0696c460b1069f43dc5eef9bd4874c1b756620
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2021-11-03 05:11:02 -0300

    atualizaçao do javascript para delete

---
+++ assets/js/javascript.js
@@ -0,0 +1,16 @@
+(function(win, doc) {
+    'use strict';
+    //verifica se user realmente quer apagar o carro
+    if (doc.querySelector('.btnDel')) {
+        let btnDel = doc.querySelectorAll('.btnDel');
+        for (let i = 0; i < btnDel.length; i++) {
+            btnDel[i].addEventListener('click', function(event) {
+                if (confirm('Deseja mesmo apagar este setor?')) {
+                    return true;
+                } else {
+                    event.preventDefault();
+                }
+            });
+        }
+    }
+})(window, document);
